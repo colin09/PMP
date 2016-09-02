b272511cd33a07cf0b45e637ae0525d74ee8fe1e
colin09 <337810174@qq.com>
2016-09-02 23:26:44 +0800

    login

---
+++ com.pmp/com.pmp.web/Controllers/AccountController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace com.pmp.web.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+        // GET: Account
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult SignIn()
+        {
+            return View();
+        }
+    }
+}
