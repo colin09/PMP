35278fad1956f2370c1503c2d2920dc5845859c1
colin.du <337810174@qq.com>
2016-09-08 18:46:44 +0800

    pcd

---
+++ com.pmp/com.pmp.mongo/service/MgCityService.cs
@@ -0,0 +1,46 @@
+﻿using com.pmp.mongo.data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace com.pmp.mongo.service
+{
+    public class MgCityService:BaseService<MgCity>
+    {
+
+
+        public List<MgCity> GetListByParentId(int parentId)
+        {
+            //var filter = bl
+            return null;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+
+    }
+
+}
