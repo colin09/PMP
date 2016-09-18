851ec4f668ed04b64abacd3e8f9bfbb9c6b8b88c
xiaoyanger1 <z_young@sina.cn>
2016-09-18 21:47:54 +0800

    add models

--- com.pmp/com.pmp.web/Controllers/WebBaseController.cs
+++ com.pmp/com.pmp.web/Controllers/WebBaseController.cs
@@ -16,20 +16,22 @@ namespace com.pmp.web.Controllers
             {
                 var user = new MgUserService().SearchById(_Longin_UserId);
                 if (user != null && user.Count > 0)
+                {
                     base._Login_CompanyReal_ID = user[0].CompanyReal_ID;
 
-                if (user[0].Level == UserLevel.Person)
-                {
-                    base._Longin_Person_IsApprove = user[0].PersonReal.IsApprove;
-
-                }
-                else if (user[0].Level == UserLevel.CompanyAdmin || user[0].Level == UserLevel.CompanyUser)
-                {
-                    if (user[0].CompanyReal_ID > -1)
+                    if (user[0].Level == UserLevel.Person)
                     {
-                        var comoany = new MgCompanyRealService().SearchById(user[0].CompanyReal_ID);
-                        if (comoany != null && comoany.Count > 0)
-                            base._Longin_CompanyReal_IsApprove = comoany[0].IsApprove;
+                        base._Longin_Person_IsApprove = user[0].PersonReal.IsApprove;
+
+                    }
+                    else if (user[0].Level == UserLevel.CompanyAdmin || user[0].Level == UserLevel.CompanyUser)
+                    {
+                        if (user[0].CompanyReal_ID > -1)
+                        {
+                            var comoany = new MgCompanyRealService().SearchById(user[0].CompanyReal_ID);
+                            if (comoany != null && comoany.Count > 0)
+                                base._Longin_CompanyReal_IsApprove = comoany[0].IsApprove;
+                        }
                     }
                 }
             }
