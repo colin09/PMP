e9a86e979e9c9b23256c7ffa59bbfb645d412ab7
colin.du <337810174@qq.com>
2017-04-12 09:27:44 +0800

    NHibernate

---
+++ com.pmp/com.pmp.nh/repository/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using com.pmp.nh.domain;
+
+namespace com.pmp.nh.repository
+{
+    public interface IRepository<T> where T : BaseModel, new()
+    {
+        void Add(T product);
+        void Update(T product);
+        void Remove(T product);
+        T GetById(Guid productId);
+        ICollection<T> GetList(string category);
+    }
+}
---
+++ com.pmp/com.pmp.nh/repository/Repository.cs
@@ -0,0 +1,75 @@
+﻿using com.pmp.nh.domain;
+using NHibernate;
+using NHibernate.Criterion;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace com.pmp.nh.repository
+{
+    public class Repository<T> : IRepository<T> where T : BaseModel, new()
+    {
+
+        public IQueryable<T> Query()
+        {
+            using (ISession session = NHibernateHelper.OpenSession())
+                return session.Query<T>();
+        }
+
+
+        public void Add(T model)
+        {
+            using (ISession session = NHibernateHelper.OpenSession())
+            using (ITransaction transaction = session.BeginTransaction())
+            {
+                session.Save(model);
+                transaction.Commit();
+            }
+        }
+
+        public ICollection<T> GetList(string category)
+        {
+            using (ISession session = NHibernateHelper.OpenSession())
+            {
+                var list = session
+                    .CreateCriteria(typeof(T))
+                    .Add(Restrictions.Eq("Category", category))
+                    .List<T>();
+                return list;
+            }
+        }
+
+        public T GetById(Guid id)
+        {
+            using (ISession session = NHibernateHelper.OpenSession())
+            {
+                var model = session
+                    .CreateCriteria(typeof(T))
+                    .Add(Restrictions.Eq("Id", id))
+                    .UniqueResult<T>();
+                return model;
+            }
+        }
+
+
+        public void Remove(T model)
+        {
+            using (ISession session = NHibernateHelper.OpenSession())
+            using (ITransaction transaction = session.BeginTransaction())
+            {
+                session.Delete(model);
+                transaction.Commit();
+            }
+        }
+
+        public void Update(T model)
+        {
+            using (ISession session = NHibernateHelper.OpenSession())
+            using (ITransaction transaction = session.BeginTransaction())
+            {
+                session.Update(model);
+                transaction.Commit();
+            }
+        }
+    }
+}
